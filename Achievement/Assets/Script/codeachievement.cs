36ecc30df2eb069fea3180cb0493f5af2b76d5d8
bigmass555 <54878538+bigmass555@users.noreply.github.com>
2019-12-09 14:18:26 +0700

    challenge change
    challenge change

---
+++ Achievement/Assets/Script/codeachievement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class codeAchievement : MonoBehaviour
+{
+    public Image imagebar;
+    public Button buttoned;
+    int num = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void press()
+    {
+        imagebar.fillAmount = num / 100f;
+    }
+}
