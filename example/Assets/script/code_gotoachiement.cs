8287831a937344d54f5d5fc0a565b36b880540bd
aff717616 <56432742+aff717616@users.noreply.github.com>
2019-12-09 13:54:55 +0700

    Achievement
    ทำหน้า Achevement

---
+++ example/Assets/script/code_gotoachiement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class code_gotoachiement : MonoBehaviour
+{
+    public GameObject empty;
+    public Button buttoned;
+    public Canvas[] canvas;
+    public TextMeshProUGUI text_;
+    public TextMeshProUGUI textScore;
+    public superman man;
+    public Image imagebar;
+    public GameObject block;
+    public GameObject panel;
+    public Image imagebartwo;
+    int num = 0;
+    public float num2 = 2.4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = empty.GetComponentsInChildren<Canvas>();
+        show_name();
+        textScore.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void show_name()
+    {
+        Debug.Log("asd");
+        foreach (Canvas kuy in canvas)
+        {
+            Debug.Log(kuy.name);
+        }
+    }
+    public void press()
+    {
+        BlockController blabla;
+        buttoned.interactable = true;
+        text_.text = 123.ToString();
+        num += 1;
+        textScore.text = num.ToString();
+        int asd = int.Parse("123");
+        int abc = (int)1.5;
+        imagebar.fillAmount = num / 100f;
+        imagebartwo.fillAmount = num / 50f;
+        Debug.Log(num / 100);
+        blabla = Instantiate(block, panel.transform).GetComponent<BlockController>();
+        blabla.change_text();
+    }
+}
---
+++ example/Assets/script/superman.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class superman : MonoBehaviour
+{
+    public int num = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num == 0)
+        {
+            num = 0;
+            Debug.Log(num);
+        }
+        else {
+            Debug.Log(num);
+        }
+    }
+    public void test()
+    {
+        Debug.Log(num);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
