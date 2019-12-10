e6d22724902e85cb969c98a3a8674c898ab21864
slendychaty <slendychaty@users.noreply.github.com>
2019-12-11 00:37:32 +0700

    Redesign and Make Calender Reponsive

--- Calender/Assets/Calender/Scripts Calender/CalendarController.cs
+++ Calender/Assets/Calender/Scripts Calender/CalendarController.cs
@@ -7,12 +7,13 @@ using TMPro;
 
 public class CalendarController : MonoBehaviour, IDragHandler, IEndDragHandler
 {
-
     public GameObject monthPage;
     public TextMeshProUGUI monthText;
     public TextMeshProUGUI yearText;
     public Color greyOut;
     public Color highlightToday;
+    RectTransform monthPagesPlaceHolder;
+    [SerializeField] float horizontalSpacing;
     [SerializeField] RectTransform rt;
     [SerializeField] float percentThreshold = 0.2f;
     [SerializeField] float easing = 1f;
@@ -23,10 +24,15 @@ public class CalendarController : MonoBehaviour, IDragHandler, IEndDragHandler
     // Start is called before the first frame update
     void Start()
     {
-        oriposition = transform.position;
+        monthPagesPlaceHolder = GetComponent<RectTransform>();
+        Debug.Log(transform.position);
+        Debug.Log(transform.localPosition);
+        oriposition = transform.localPosition;
+        instantiate_month_page(-1, 0);
+        instantiate_month_page(0, 1);
+        instantiate_month_page(1, 2);
         write_calendar(Helper.Time.SubtractDate(Helper.Time.FullDate, index), 1);
         change_text(index);
-        Debug.Log(Helper.Time.GetMonthName(Helper.Time.FullDate));
     }
 
     // Update is called once per frame
@@ -66,13 +72,13 @@ public class CalendarController : MonoBehaviour, IDragHandler, IEndDragHandler
             float differ = (data.pressPosition.x - data.position.x) * swipeSpeed;
             if (differ > 0)
             {
-                write_calendar(Helper.Time.SubtractDate(Helper.Time.FullDate, index + 1), 2);
+                //write_calendar(Helper.Time.SubtractDate(Helper.Time.FullDate, index), 2);
             }
             else if (differ < 0)
             {
-                write_calendar(Helper.Time.SubtractDate(Helper.Time.FullDate, index - 1), 0);
+                //write_calendar(Helper.Time.SubtractDate(Helper.Time.FullDate, index - 1), 0);
             }
-            transform.position = oriposition - new Vector3(differ, 0, 0);
+            transform.localPosition = oriposition - new Vector3(differ, 0);
         }
     }
     public void OnEndDrag(PointerEventData data)
@@ -82,53 +88,62 @@ public class CalendarController : MonoBehaviour, IDragHandler, IEndDragHandler
             float percent = (data.pressPosition.x - data.position.x) / Screen.width;
             if (Mathf.Abs(percent) > percentThreshold)
             {
-                Vector3 newposition = oriposition;
+                Vector2 newposition = oriposition;
                 if (percent > 0)
                 {
                     index += 1;
-                    instantiate_month_page(1, 0, 3);
+                    instantiate_month_page(1, 3, 0);
                     change_text(index);
-                    newposition += new Vector3(-375, 0, 0);
+                    newposition += new Vector2(-(monthPagesPlaceHolder.rect.width + horizontalSpacing), transform.localPosition.y);
+                    Debug.Log((monthPagesPlaceHolder.rect.width + horizontalSpacing));
                 }
                 else if (percent < 0)
                 {
                     index -= 1;
-                    instantiate_month_page(-1, 2, 0);
+                    instantiate_month_page(-1, 0, 2);
                     change_text(index);
-                    newposition += new Vector3(375, 0, 0);
+                    newposition += new Vector2(monthPagesPlaceHolder.rect.width + horizontalSpacing, transform.localPosition.y);
                 }
-                StartCoroutine(smoothMove(transform.position, newposition, easing));
+                StartCoroutine(smoothMove(transform.localPosition, newposition, easing));
                 oriposition = newposition;
             }
             else
             {
-                StartCoroutine(smoothMove(transform.position, oriposition, easing));
+                StartCoroutine(smoothMove(transform.localPosition, oriposition, easing));
             }
         }
     }
-    void instantiate_month_page(int direction, int destroyindex, int childindex)
+    void instantiate_month_page(int direction, int childindex, int destroyindex=-1)
     {
         GridLayoutGroup[] monthPages = gameObject.GetComponentsInChildren<GridLayoutGroup>();
-        Destroy(monthPages[destroyindex].gameObject);
+        if (destroyindex != -1)
+                Destroy(monthPages[destroyindex].gameObject);
         GameObject newMonthPages = Instantiate(monthPage, transform);
+        RectTransform newMonthPagesRect = newMonthPages.GetComponent<RectTransform>();
+        GridLayoutGroup layoutGroup = newMonthPages.GetComponent<GridLayoutGroup>();
+        newMonthPagesRect.sizeDelta = new Vector2(monthPagesPlaceHolder.rect.width, monthPagesPlaceHolder.rect.height);
+        layoutGroup.cellSize = new Vector2(newMonthPagesRect.sizeDelta.x / 7, newMonthPagesRect.sizeDelta.y / 6);
         newMonthPages.transform.SetSiblingIndex(childindex);
-        newMonthPages.transform.localPosition = new Vector3(375 * (index + direction), 0, 0);
+        write_calendar(Helper.Time.SubtractDate(Helper.Time.FullDate, index + direction), childindex);
+        newMonthPages.transform.localPosition = new Vector3((monthPagesPlaceHolder.rect.width + horizontalSpacing) * (index + direction), 0, 0);
     }
     void change_text(int num)
     {
         monthText.text = Helper.Time.FullDate.AddMonths(num).ToString("MMMM");
         yearText.text = Helper.Time.FullDate.AddMonths(num).ToString("yyyy");
     }
-    IEnumerator smoothMove(Vector3 position, Vector3 newposition, float time)
+    IEnumerator smoothMove(Vector2 position, Vector2 newposition, float time)
     {
         float elastime = 0f;
         transitioning = true;
         while (elastime <= 1)
         {
             elastime += Time.deltaTime / time;
-            transform.position = Vector3.Lerp(position, newposition, Mathf.SmoothStep(0f, 1f, elastime));
+            transform.localPosition = Vector2.Lerp(position, newposition, Mathf.SmoothStep(0f, 1f, elastime));
             yield return null;
         }
+        Debug.Log(transform.position);
+        Debug.Log(transform.localPosition);
         transitioning = false;
     }
 }
