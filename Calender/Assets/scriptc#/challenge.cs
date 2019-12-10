fde2a48fac280ee3485a5dd49bd5dd299f4f2114
Chawunvit <54878300+Chawunvit@users.noreply.github.com>
2019-12-10 17:32:35 +0700

    update
    update scene challenge

---
+++ Calender/Assets/scriptc#/challenge.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class challenge
+{
+    public string name;
+    public string detail;
+    public int difficulty;
+    public int[] days;
+}
--- Calender/Assets/scriptc#/challenge_butt.cs
+++ Calender/Assets/scriptc#/challenge_butt.cs
@@ -9,16 +9,13 @@ public class challenge_butt : MonoBehaviour
     public float hide_x, hide_y;
     public create_post create_Post_script;
     public GameObject ccc_score_butt;
-    public GameObject crc_score_butt;
     public GameObject dl_score_butt;
     public GameObject selected_parent;
     void Start()
     {
         sub_bg.SetActive(false);
         ccc_bg.SetActive(false);
-        crc_bg.SetActive(false);
         dl_bg.SetActive(false);
-
         hide_x = bg_textdetail.transform.position.x;
         hide_y = bg_textdetail.transform.position.y;
     }
@@ -51,16 +48,6 @@ public class challenge_butt : MonoBehaviour
         ccc_bg.SetActive(false);
     }
 
-    public void button_to_create_random()
-    {
-        crc_bg.SetActive(true);
-        selected_parent = crc_score_butt;
-    }
-    public void button_cancle_create_random()
-    {
-        crc_bg.SetActive(false);
-    }
-
     public void button_to_deadline()
     {
         dl_bg.SetActive(true);
@@ -75,7 +62,6 @@ public class challenge_butt : MonoBehaviour
     {
         sub_bg.SetActive(false);
         ccc_bg.SetActive(false);
-        crc_bg.SetActive(false);
         dl_bg.SetActive(false);
     }
 
